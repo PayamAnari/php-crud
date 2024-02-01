d6749e64b0965c62229518cf5bda9ebe7b5c5d07
PayamAnari <anari.p62@gmail.com>
2024-02-01 21:49:36 +0100

    config database

---
+++ config/Database.php
@@ -0,0 +1,13 @@
+<?php
+
+class Database
+{
+
+    // Database Properties.
+
+    private $host = "localhost";
+    private $db_name = "php-crud";
+    private $username = "root";
+    private $password = "";
+    private $connection;
+}
