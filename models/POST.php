740b4a9cf3c3b999cffb1aafc16a5dd9e3e14d05
PayamAnari <anari.p62@gmail.com>
2024-02-01 22:18:49 +0100

    modify readposts func

--- models/POST.php
+++ models/POST.php
@@ -26,4 +26,26 @@ class Post
         $this->connection = $db;
     }
 
+    //Get Posts.
+
+    public function readPosts()
+    {
+
+        //Create Query.
+
+        $query = 'SELECT
+      category.name as category,
+      posts.id,
+      posts.title,
+      posts.description,
+      posts.category_id,
+      posts.created_at
+      FROM ' . $this->$table . ' posts LEFT JOIN
+      category ON posts.category_id = category.id
+      ORDER BY
+      posts.created_at DESC
+      ';
+
+    }
+
 }
