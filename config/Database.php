adc8671744801c9fbc37616846c1fcd1308b8c85
PayamAnari <anari.p62@gmail.com>
2024-02-01 21:58:39 +0100

    Add connect

--- config/Database.php
+++ config/Database.php
@@ -9,5 +9,20 @@ class Database
     private $db_name = "php-crud";
     private $username = "root";
     private $password = "";
-    private $connection;
+    private $connection = null;
+
+    // Function for Database Connection.
+
+    public function connect()
+    {
+
+        try {
+            $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->connection->exec("set names utf8");
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+        return $this->connection;
+    }
 }
