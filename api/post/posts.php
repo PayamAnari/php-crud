49cd5fdabe3df55422b6be4bca1b7304d54bc0c9
PayamAnari <anari.p62@gmail.com>
2024-02-02 11:25:30 +0100

    Modify posts

--- api/post/posts.php
+++ api/post/posts.php
@@ -1,1 +1,10 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//Headers
+
+Header('Access-Control-Allow-Origin: *');
+Header('Content-Type: application/json');
+Header('Access-Control-Allow-Methods: POST');
