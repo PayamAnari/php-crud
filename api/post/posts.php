44df125003cff6960eee6b6d8066457f430026a2
PayamAnari <anari.p62@gmail.com>
2024-02-02 11:44:09 +0100

    Modify posts

--- api/post/posts.php
+++ api/post/posts.php
@@ -8,3 +8,42 @@ ini_set('display_errors', 1);
 Header('Access-Control-Allow-Origin: *');
 Header('Content-Type: application/json');
 Header('Access-Control-Allow-Methods: POST');
+
+//Include Database and Post Model.
+
+include_once '../../config/Database.php';
+include_once '../../models/Post.php';
+
+//Instantiate Database.
+
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate Post.
+
+$post = new Post($db);
+$data = $post->readPosts();
+
+//if there is posts available
+
+if ($data->rowCount()) {
+    $posts -= [];
+
+    //fetching the data from the database
+
+    while ($row = $data->fetch(PDO::FETCH_OBJ)) {
+       
+        $posts[$row->id] = [
+            'id' => $row->id,
+            'categoryName' => $row->category,
+            'description' => $row->description,
+            'title' => $row->title,
+            'created_at' => $row->created_at          
+        ]
+    }
+
+} else {
+    echo json_encode(
+        ['message' => 'No Posts Found']
+    );
+}
