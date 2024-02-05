b28016fb46a742cc55586b01a12192a79501e6e0
PayamAnari <anari.p62@gmail.com>
2024-02-05 23:13:26 +0100

    Add swagger.json

--- api/documentation/api.php
+++ api/documentation/api.php
@@ -3,5 +3,7 @@ require "../../vendor/autoload.php";
 
 $openapi = \OpenApi\Generator::scan([$_SERVER['DOCUMENT_ROOT'] . '/models']);
 
+$openapi->saveAs($_SERVER['DOCUMENT_ROOT'] . '/swagger.json');
+
 header('Content-Type: application/json');
 echo $openapi->toJSON();
