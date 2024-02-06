330d97f470ba92dfe07b75a012033d9d6536901c
PayamAnari <anari.p62@gmail.com>
2024-02-06 12:45:47 +0100

    Modify

--- api/documentation/api.php
+++ api/documentation/api.php
@@ -3,7 +3,7 @@ require "../../vendor/autoload.php";
 
 $openapi = \OpenApi\Generator::scan([$_SERVER['DOCUMENT_ROOT'] . '/models']);
 
-$openapi->saveAs($_SERVER['DOCUMENT_ROOT'] . '/swagger.json');
+$openapi->saveAs($_SERVER['DOCUMENT_ROOT'] . 'dist/swagger.json');
 
 header('Content-Type: application/json');
 echo $openapi->toJSON();
